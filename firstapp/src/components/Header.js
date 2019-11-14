154efe06faa62c1f53eda99736b01faabeca3623
aakashDeveloper <ahanda205@gmail.com>
2019-11-14 01:21:15 +0000

    a

--- firstapp/src/components/Header.js
+++ firstapp/src/components/Header.js
@@ -1,6 +1,41 @@
-import React from 'react';
+import React,{ Component} from 'react';
 import './Header.css';
 
+class Header extends Component {
+    constructor(){
+        super()
+
+        this.state={
+            title: 'React App',
+            keyWords:''
+        }
+    }
+
+    inputChange(event){
+        // console.log(event.target.value)
+        this.setState({keyWords:event.target.value})
+    }
+    render(){
+        return(
+            <header >
+                <h1 className="logo"
+                onClick={()=> console.log('clicked')}
+                >{this.state.title}</h1>
+                <center>
+                    <input type="text" onChange={this.inputChange.bind(this)}/>
+                    <h3>{this.state.keyWords}</h3>
+                </center>
+                <hr/>
+            </header>
+        )
+    }
+}
+
+export default Header;
+
+
+
+/*
 const Header = () => {
     return(
         <div>
@@ -12,5 +47,4 @@ const Header = () => {
         </div>
     )
 }
-
-export default Header;
+*/
